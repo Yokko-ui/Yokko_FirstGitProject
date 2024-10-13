95fed4ef449642417de5347e9354e1901be22e68
Kazuhiro Yokoyama <yokko-marching21@outlook.jp>
2024-10-13 23:46:46 +0900

    5章まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,9 +1,46 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+export type Category = {
+  name: string;
+} & MicroCMSListContent;
+
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
+
+const client = createClient({
+  serviceDomain: "domain_input",
+  apiKey: "key_input",
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
+  return listData;
 };
